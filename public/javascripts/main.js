cc2448f54c2765fac94e9a6c1f59e12ff8e07e1b
Ruby Turbek <Rubek@Rubys-MacBook-Pro.local>
2014-03-07 00:23:36 -0500

    svg prototype

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -32,7 +32,7 @@ SectionView = Backbone.View.extend({
             e.target.innerText = '';
         } else {
             e.target.innerText = e.target.innerHTML;
-        } 
+        }
     },
     endEdit: function(e){
       console.log('endEdit')
@@ -62,12 +62,12 @@ SectionView = Backbone.View.extend({
             }
 
         }
-        
+
       } else {
         e.target.innerHTML = e.target.innerText;
       }
 
-      
+
     }
 });
 
@@ -107,6 +107,23 @@ var page = new SectionList();
 var newSect = new NewSection({ el : $("#new") });
 
 
+/*
+$('body').on('click', '.draw', function(){
+    console.log(this)
+    if( $(this).parent().find('.media svg').length < 1 ){
+        $(this).parent().find('.media').removeClass('new');
+        $(this).parent().find('.media').html('<svg id="paper" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink"http://www.w3.org/1999/xlink" attributeType="XML"></svg>');
+
+        var draws = [];
+        draws[0] = new Drawing(document.getElementById('paper'));
+        $('#paper').on('mouseup', function(){
+            draws[draws.length] = new Drawing(document.getElementById('paper'));
+        });
+
+
+    }
+});
+*/
 
 function saveFilter(el){
     var value = false;
@@ -146,13 +163,13 @@ var Save = Backbone.View.extend({
                 m.description = description;
             }
             if( $(sections[i]).find('.media img') ){
-                m.media = $(sections[i]).find('.media img').attr('src')                
+                m.media = $(sections[i]).find('.media img').attr('src')
             }
 
             items.push(m);
         }
 
-        var data = { 
+        var data = {
             name : name,
             entry : {
                 "Version" : parseInt($('#latest').attr('data-version')),
@@ -195,7 +212,7 @@ $('#custom-styles').on('focus', function(){
 
 $('#custom-styles').on('blur', function(){
     $(document).off('keydown', addTabFormat);
-}); 
+});
 
 function addTabFormat(e){
     if(e.keyCode === 9) {
@@ -232,3 +249,69 @@ $('#prev').on('click', function(){
 $('#next').on('click', function(){
     window.mySwipe.next();
 });
+
+
+
+
+
+
+
+
+function Drawing(paper) {
+    var self = this,
+        startRecording,
+        g = newSVG('path'),
+        x,y,
+        pts = [],
+        attr = '',
+        bGrabbing = false;
+
+    g.setAttributeNS(null,"stroke","black");
+    g.setAttributeNS(null,"fill","none");
+    g.setAttributeNS(null,"stroke-width","3");
+
+    paper.addEventListener('mousedown', beginDraw, false);
+    paper.addEventListener('mouseup', endDraw, false);
+    paper.addEventListener('mousemove', function(e){
+        x = e.offsetX;
+        y = e.offsetY;
+    }, false);
+
+
+    function beginDraw(e){
+        if(!bGrabbing){
+            attr += "M " + e.offsetX + " " + e.offsetY;
+            g.setAttributeNS(null,"d", attr);
+            paper.appendChild(g);
+            startRecording = setInterval(record, 16);
+        }
+    }
+    function endDraw(e){
+        clearInterval(startRecording)
+        paper.removeEventListener('mousedown', beginDraw);
+    }
+    function record(){
+        attr += " L " + x + " " + y;
+        g.setAttributeNS(null,"d", attr);
+    }
+
+
+    g.addEventListener('mousedown', function(){
+    //    console.log('down')
+    }, false);
+
+    g.addEventListener('mouseup', function(){
+    //    console.log('up')
+    }, false);
+
+    function newSVG(type){
+        return document.createElementNS("http://www.w3.org/2000/svg", type);
+    }
+
+}
+
+
+
+
+
+
---
+++ public/prototypes/drawRecorder.js
@@ -0,0 +1,58 @@
+function Drawing(paper) {
+    var self = this,
+        startRecording,
+        g = newSVG('path'),
+        x,y,
+        pts = [],
+        attr = '',
+        bGrabbing = false;
+
+    g.setAttributeNS(null,"stroke","black");
+    g.setAttributeNS(null,"fill","none");
+    g.setAttributeNS(null,"stroke-width","3");
+
+    paper.addEventListener('mousedown', beginDraw, false);
+    paper.addEventListener('mouseup', endDraw, false);
+    paper.addEventListener('mousemove', function(e){
+        x = e.offsetX;
+        y = e.offsetY;
+    }, false);
+
+
+    function beginDraw(e){
+        if(!bGrabbing){
+            attr += "M " + e.offsetX + " " + e.offsetY;
+            g.setAttributeNS(null,"d", attr);
+            paper.appendChild(g);
+            startRecording = setInterval(record, 16);
+        }
+    }
+    function endDraw(e){
+        clearInterval(startRecording)
+        paper.removeEventListener('mousedown', beginDraw);
+    }
+    function record(){
+        attr += " L " + x + " " + y;
+        g.setAttributeNS(null,"d", attr);
+    }
+
+
+    g.addEventListener('mousedown', function(){
+    //    console.log('down')
+    }, false);
+
+    g.addEventListener('mouseup', function(){
+    //    console.log('up')
+    }, false);
+
+    function newSVG(type){
+        return document.createElementNS("http://www.w3.org/2000/svg", type);
+    }
+
+}
+
+
+
+
+
+
